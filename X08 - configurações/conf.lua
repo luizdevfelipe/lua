62c47ad14c3950fcc928267fc0f05a21404e0a37
Luiz Felipe <118226614+luizdevfelipe@users.noreply.github.com>
2025-03-11 17:12:58 -0300

    gerenciamento dos módulos existentes

--- X08 - configurações/conf.lua
+++ X08 - configurações/conf.lua
@@ -23,4 +23,19 @@ function love.conf(t)
   t.window.x = nil
   t.window.y = nil 
   
+  t.modules.audio = true
+  t.modules.event = true
+  t.modules.graphics = true
+  t.modules.image = true
+  t.modules.joystick = true
+  t.modules.math = true
+  t.modules.mouse = true
+  t.modules.physics = true
+  t.modules.sound = true
+  t.modules.system = true
+  t.modules.timer = true
+  t.modules.touch = true
+  t.modules.video = true
+  t.modules.window = true
+  t.modules.threads = true
 end
