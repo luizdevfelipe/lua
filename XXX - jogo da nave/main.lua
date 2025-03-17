a5364943763ce7853eaa897440b6a73bb560cd62
Luiz Felipe <118226614+luizdevfelipe@users.noreply.github.com>
2025-03-17 16:40:44 -0300

    disparando os projéteis

--- XXX - jogo da nave/main.lua
+++ XXX - jogo da nave/main.lua
@@ -2,16 +2,47 @@ larguraTela = love.graphics.getWidth()
 alturaTela = love.graphics.getHeight()
 
 function love.load()
-  imgNave = love.graphics.newImage("imagens/nave.png")
-  
+  -- Nave
+  imgNave = love.graphics.newImage("imagens/nave.png")  
   nave = {
       posX = larguraTela / 2,
       posY = alturaTela / 2,
       velocidade = 200
   }
+  -- Tiros
+  atira = true
+  delayTiro = 0.1
+  tempoAteAtirar = delayTiro
+  tiros = {}
+  imgTiro = love.graphics.newImage("imagens/projetil.png")
 end
 
 function love.update(dt)
+  movimentos(dt)
+  atirar(dt)
+end
+
+function atirar(dt)
+  tempoAteAtirar = tempoAteAtirar - (1 * dt)
+  if tempoAteAtirar < 0 then
+    atira = true
+  end
+  
+  if love.keyboard.isDown("space") and atira then
+    novoTiro = {x = nave.posX, y = nave.posY, img = imgTiro}
+    table.insert(tiros, novoTiro)
+    atira = false
+    tempoAteAtirar = delayTiro
+  end
+  for i, tiro in ipairs(tiros) do
+    tiro.y = tiro.y - 500 * dt
+    if tiro.y < 0 then
+      table.remove(tiros, i)
+    end
+  end
+end
+
+function movimentos (dt)
   if love.keyboard.isDown("right") and nave.posX < larguraTela - imgNave:getWidth() / 2 then
     nave.posX = nave.posX + nave.velocidade * dt
   end
@@ -30,5 +61,14 @@ function love.update(dt)
 end
 
 function love.draw()
+  -- Nave
   love.graphics.draw(imgNave, nave.posX, nave.posY, 0, 1, 1, imgNave:getWidth() / 2, imgNave:getHeight() / 2)
+  -- Nave
+  
+  -- Tiros
+    for i, tiro in ipairs(tiros) do
+      love.graphics.draw(tiro.img, tiro.x, tiro.y, 0, 1, 1, imgTiro:getWidth() / 2, imgTiro:getHeight())
+    end
+  -- Tiros
+  
 end
