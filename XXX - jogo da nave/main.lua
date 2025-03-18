d0ccd5482cbb51698b911d366a121fd1fbc48cb1
Luiz Felipe <118226614+luizdevfelipe@users.noreply.github.com>
2025-03-18 16:44:52 -0300

    detectando colisões entre tiros, inimigos e o personagem
    Com essa detecção inimigos são destruídos, pontos são somados e o jogador pode perder se encontar em um inimigo

--- XXX - jogo da nave/main.lua
+++ XXX - jogo da nave/main.lua
@@ -25,12 +25,51 @@ function love.load()
   imgInimigo = love.graphics.newImage("imagens/inimigo.png")
   inimigos = {}
   -- Inimigo
+  
+  -- Vidas e Pontuação
+  estaVivo = true
+  pontos = 0
+  -- Vidas e Pontuação
 end
 
 function love.update(dt)
   movimentos(dt)
   atirar(dt)
   inimigo(dt)
+  colisoes()
+  
+  if not estaVivo and love.keyboard.isDown("return") then
+    tiros = {}
+    inimigos = {}
+    tempoAteAtirar = delayTiro
+    tempoCriarInimigo = delayInimigo
+    
+    nave.posX = larguraTela / 2
+    nave.posY = alturaTela / 2
+    
+    pontos = 0
+    estaVivo = true    
+  end
+end
+
+function love.draw()
+  -- Tiros
+    for i, tiro in ipairs(tiros) do
+      love.graphics.draw(tiro.img, tiro.x, tiro.y, 0, 1, 1, imgTiro:getWidth() / 2, imgTiro:getHeight())
+    end
+  -- Tiros
+  -- Inimigos
+    for i, inimigo in ipairs(inimigos) do
+      love.graphics.draw(inimigo.img, inimigo.x, inimigo.y)
+    end
+  -- Inimigos
+  -- Game Over e Reset
+  if estaVivo then
+    love.graphics.draw(imgNave, nave.posX, nave.posY, 0, 1, 1, imgNave:getWidth() / 2, imgNave:getHeight() / 2)
+  else
+    love.graphics.print("Aperte 'Enter' para reiniciar...", larguraTela / 3, alturaTela / 2)
+  end
+  -- Game Over e Reset
 end
 
 function atirar(dt)
@@ -39,7 +78,7 @@ function atirar(dt)
     atira = true
   end
   
-  if love.keyboard.isDown("space") and atira then
+  if estaVivo and love.keyboard.isDown("space") and atira then
     novoTiro = {x = nave.posX, y = nave.posY, img = imgTiro}
     table.insert(tiros, novoTiro)
     atira = false
@@ -71,28 +110,11 @@ function movimentos (dt)
   end
 end
 
-function love.draw()
-  -- Nave
-  love.graphics.draw(imgNave, nave.posX, nave.posY, 0, 1, 1, imgNave:getWidth() / 2, imgNave:getHeight() / 2)
-  -- Nave
-  -- Tiros
-    for i, tiro in ipairs(tiros) do
-      love.graphics.draw(tiro.img, tiro.x, tiro.y, 0, 1, 1, imgTiro:getWidth() / 2, imgTiro:getHeight())
-    end
-  -- Tiros
-  -- Inimigos
-    for i, inimigo in ipairs(inimigos) do
-      love.graphics.draw(inimigo.img, inimigo.x, inimigo.y)
-    end
-  -- Inimigos
-  
-end
-
 function inimigo(dt)
   tempoCriarInimigo = tempoCriarInimigo - 1 * dt
   if tempoCriarInimigo < 0 then
     tempoCriarInimigo = delayInimigo
-    numeroAleatorio = math.random(10, love.graphics.getWidth() - imgInimigo:getWidth() / 2 + 10 )
+    numeroAleatorio = math.random(10, love.graphics.getWidth() - imgInimigo:getWidth() / 2)
     novoInimigo = {x = numeroAleatorio, y = -imgInimigo:getHeight(), img = imgInimigo}
     table.insert(inimigos, novoInimigo)
   end
@@ -104,3 +126,22 @@ function inimigo(dt)
     end
   end
 end
+function colisoes()
+  for i, inimigo in ipairs(inimigos) do
+    for j, tiro in ipairs(tiros) do
+      if checarColisao(inimigo.x, inimigo.y, imgInimigo:getWidth(), imgInimigo:getHeight(), tiro.x, tiro.y, imgTiro:getWidth(), imgTiro:getHeight()) then
+        table.remove(tiros, j)
+        table.remove(inimigos, i)
+        pontos = pontos + 1
+      end
+    end
+    if checarColisao(inimigo.x, inimigo.y, imgInimigo:getWidth(), imgInimigo:getHeight(), nave.posX - imgNave:getWidth() / 2, nave.posY, imgNave:getWidth(), imgNave:getHeight()) and estaVivo then
+      table.remove(inimigo, i)
+      estaVivo = false
+    end
+  end
+end 
+function checarColisao(x1, y1, w1, h1, x2, y2, w2, h2)
+  return x1 < x2 + w2 and x2 < x1 + w1 and y1 < y2 + h2 and y2 < y1 + h1
+end
+  
