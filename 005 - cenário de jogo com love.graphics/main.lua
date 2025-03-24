821650974846fd8e9545d2c7ad229927045329eb
Luiz Felipe <118226614+luizdevfelipe@users.noreply.github.com>
2025-03-24 20:59:07 -0300

    cenário feito com funções de desenho do módulo graphics

---
+++ 005 - cenário de jogo com love.graphics/main.lua
@@ -0,0 +1,52 @@
+function love.draw()
+
+-- Desenha o chão
+love.graphics.setColor(0, 154, 10)
+love.graphics.rectangle("fill", 0, 500, 800, 100)
+
+-- Desenha a árvore
+-- tronco
+love.graphics.setColor(139, 69, 19)
+love.graphics.rectangle("fill", 600, 350, 40, 150)
+-- copa
+love.graphics.setColor(0, 184, 10)
+love.graphics.circle("fill", 620, 300, 80, 100)
+
+-- Desenha a casa
+-- frente
+love.graphics.setColor(150, 150, 150)
+love.graphics.rectangle("fill", 200, 300, 200, 200)
+-- janela
+love.graphics.setColor(10, 10, 190)
+love.graphics.rectangle("fill", 220, 390, 50, 50)
+-- porta
+love.graphics.setColor(139, 69, 19)
+love.graphics.rectangle("fill", 300, 380, 70, 120)
+-- maçaneta
+love.graphics.setColor(10, 10, 10)
+love.graphics.circle("fill", 315, 450, 4, 100)
+-- Teto
+love.graphics.setColor(150, 10, 10)
+love.graphics.polygon("fill", 200, 300, 400, 300, 300, 200, 200, 300)
+
+-- O Sol
+love.graphics.setColor(252, 212, 64)
+love.graphics.circle("fill", 150, 100, 45, 100)
+-- Raios do Sol
+love.graphics.setLineWidth(5) -- determina a espessura do raio de Sol
+love.graphics.line(150, 20, 150, 180)
+love.graphics.line(70, 100, 230, 100)
+love.graphics.line(90, 40, 210, 160)
+love.graphics.line(210, 40, 90, 160)
+
+-- Nuvem
+love.graphics.setColor(240, 240, 240)
+love.graphics.circle("fill", 460, 100, 40, 100)
+love.graphics.circle("fill", 500, 100, 50, 100)
+love.graphics.circle("fill", 540, 100, 50, 100)
+love.graphics.circle("fill", 580, 100, 40, 100)
+
+-- Ajusta a cor do fundo
+love.graphics.setBackgroundColor(135, 206, 235)
+
+end
